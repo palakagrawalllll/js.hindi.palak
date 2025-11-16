8cad492f6044e7a478fee8920eb7f3b8dd6894e5
palakagrawalllll <palakagrawal020406@gmail.com>
2025-11-16 16:59:08 +0000

    learned about const, let, var

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,8 @@
+const accountId = 123456
+let accountEmail = "palakagrawal02837@gamil.com"
+var accountPassword = "palak@8672"
+accountCity = "Jhansi"
+let accountState;
+
+console.log(accountId)
+console.table({accountEmail, accountPassword, accountCity, accountState})
